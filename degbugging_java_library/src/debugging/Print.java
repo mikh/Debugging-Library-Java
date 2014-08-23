bd9bf48fa155b0409cb637d43c11a0390e764006
Mikhail Andreev <mikha.andreev@gmail.com>
2014-08-22 20:07:04 -0400

    Added documentation to print and created jar file.

--- degbugging_java_library/src/debugging/Print.java
+++ degbugging_java_library/src/debugging/Print.java
@@ -2,7 +2,13 @@ package debugging;
 
 import java.util.ArrayList;
 
+
 public class Print {
+	
+	/**
+	 * Prints out ArrayList<ArrayList<String>> to the console
+	 * @param aas - input ArrayList<ArrayList<String>>
+	 */
 	public static void print(ArrayList<ArrayList<String>> aas){
 		for(int ii = 0; ii < aas.size(); ii++){
 			for(int jj= 0; jj < aas.get(ii).size(); jj++){
